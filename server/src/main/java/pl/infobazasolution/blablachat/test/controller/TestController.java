d32984b50d761608c4e4386027fdfbe37f4fc796
g.pawlak <g.pawlak@info-baza.pl>
2020-09-30 11:19:04 +0200

    TASK-J-1 - init skeleton

---
+++ server/src/main/java/pl/infobazasolution/blablachat/test/controller/TestController.java
@@ -0,0 +1,16 @@
+package pl.infobazasolution.blablachat.test.controller;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.time.LocalDateTime;
+
+@Path("/test")
+public class TestController {
+
+    @GET
+    @Path("/ping")
+    public String ping(){
+        return LocalDateTime.now().toString();
+    }
+
+}
