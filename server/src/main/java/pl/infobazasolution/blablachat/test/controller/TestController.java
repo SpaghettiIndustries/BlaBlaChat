a25893312b8c134178c952801036c2f83513ada7
praktyka1 <praktyka1@info-baza.pl>
2020-10-01 14:15:01 +0200

    Task-J-1 - Stworzenie struktury projektu

--- server/src/main/java/pl/infobazasolution/blablachat/test/controller/TestController.java
+++ server/src/main/java/pl/infobazasolution/blablachat/test/controller/TestController.java
@@ -2,15 +2,15 @@ package pl.infobazasolution.blablachat.test.controller;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.PathParam;
 import java.time.LocalDateTime;
 
 @Path("/test")
 public class TestController {
-
     @GET
     @Path("/ping")
     public String ping(){
         return LocalDateTime.now().toString();
     }
-
 }
