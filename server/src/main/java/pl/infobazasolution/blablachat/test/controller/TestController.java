d23100146c5ec0bfa707d1f6ed10562598595c62
praktyka2 <praktyka2@info-baza.pl>
2020-10-05 12:16:26 +0200

    TASK-J-3 Wstępna implementacja JWT

--- server/src/main/java/pl/infobazasolution/blablachat/test/controller/TestController.java
+++ server/src/main/java/pl/infobazasolution/blablachat/test/controller/TestController.java
@@ -1,5 +1,7 @@
 package pl.infobazasolution.blablachat.test.controller;
 
+import pl.infobazasolution.blablachat.security.jwt.JWTTokenNeeded;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
@@ -10,6 +12,7 @@ import java.time.LocalDateTime;
 public class TestController {
     @GET
     @Path("/ping")
+    @JWTTokenNeeded
     public String ping(){
         return LocalDateTime.now().toString();
     }
