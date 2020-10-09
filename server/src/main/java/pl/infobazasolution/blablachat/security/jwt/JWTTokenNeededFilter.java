f62ab165899e80c8cea9dc25a984ca49cc208f75
praktyka2 <praktyka2@info-baza.pl>
2020-10-09 09:59:54 +0200

    TASK-J-3 Zmieniono miejsce przechowywania tokenu z nagłowka na ciasteczko

--- server/src/main/java/pl/infobazasolution/blablachat/security/jwt/JWTTokenNeededFilter.java
+++ server/src/main/java/pl/infobazasolution/blablachat/security/jwt/JWTTokenNeededFilter.java
@@ -2,6 +2,7 @@ package pl.infobazasolution.blablachat.security.jwt;
 
 import io.jsonwebtoken.Jwts;
 import org.apache.log4j.Logger;
+import pl.infobazasolution.blablachat.common.exception.AuthenticationException;
 import pl.infobazasolution.blablachat.security.jwt.util.KeyGenerator;
 
 import javax.annotation.Priority;
@@ -10,6 +11,7 @@ import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
@@ -26,15 +28,20 @@ public class JWTTokenNeededFilter implements ContainerRequestFilter {
     private KeyGenerator keyGenerator;
 
     @Override
-    public void filter(ContainerRequestContext containerRequestContext) throws IOException {
-        String authorizationHeader = containerRequestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+    public void filter(ContainerRequestContext containerRequestContext) {
+        /*String authorizationHeader = containerRequestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
 
         if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
             containerRequestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
             throw new NotAuthorizedException("Authorization header must be provided");
         }
 
-        String token = authorizationHeader.substring("Bearer".length()).trim();
+        String token = authorizationHeader.substring("Bearer".length()).trim();*/
+
+        String token = containerRequestContext.getCookies().get("token").getValue();
+
+        if (token == null)
+            containerRequestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
 
         try {
             Key key = keyGenerator.generateKey();
