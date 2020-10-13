7c2b27248dee667a84ad41f97940fafe34720ac3
praktyka2 <praktyka2@info-baza.pl>
2020-10-13 12:35:14 +0200

    TASK-J-1 Dodano narzędzia do walidacji istnienia użytkowników oraz konwersacji (tematów) w bazie danych

--- server/src/main/java/pl/infobazasolution/blablachat/common/util/ValidationUtils.java
+++ server/src/main/java/pl/infobazasolution/blablachat/common/util/ValidationUtils.java
@@ -1,6 +1,16 @@
 package pl.infobazasolution.blablachat.common.util;
 
+import org.apache.commons.validator.EmailValidator;
+import pl.infobazasolution.blablachat.component.topic.dao.TopicDao;
+import pl.infobazasolution.blablachat.component.topic.entity.Topic;
+import pl.infobazasolution.blablachat.component.user.dao.UserDao;
+import pl.infobazasolution.blablachat.component.user.entity.User;
+
+import javax.inject.Inject;
+import java.util.List;
+
 public class ValidationUtils {
+
     public static Boolean containsWhitespace(String str) {
         if (!(str != null && str.length() > 0)) {
             return false;
@@ -15,5 +25,37 @@ public class ValidationUtils {
         }
 
         return false;
+    }
+
+    public static Boolean isValidEmail(String email) {
+        EmailValidator validator = EmailValidator.getInstance();
+
+        return validator.isValid(email);
+    }
+
+    public static Boolean userExists(Integer id, UserDao userDao) {
+        List<User> existingUsers = userDao.readAll();
+
+        Boolean exists = false;
+
+        for (User existingUser : existingUsers) {
+            if (existingUser.getId().equals(id))
+                exists = true;
+        }
+
+        return exists;
+    }
+
+    public static Boolean topicExists(Integer id, TopicDao topicDao) {
+        List<Topic> existingTopics = topicDao.readAll();
+
+        Boolean exists = false;
+
+        for (Topic existingTopic : existingTopics) {
+            if (existingTopic.getId().equals(id))
+                exists = true;
+        }
+
+        return exists;
     }
 }
