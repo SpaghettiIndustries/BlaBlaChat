add43144ead82673bb8f01cf469a47b7359554ad
praktyka2 <praktyka2@info-baza.pl>
2020-10-15 08:58:55 +0200

    Usunięcie niepotrzebnych elementów z kontrolera testowego

--- server/src/main/java/pl/infobazasolution/blablachat/test/controller/TestController.java
+++ server/src/main/java/pl/infobazasolution/blablachat/test/controller/TestController.java
@@ -1,26 +1,14 @@
 package pl.infobazasolution.blablachat.test.controller;
 
-import pl.infobazasolution.blablachat.component.user.dao.UserDao;
-import pl.infobazasolution.blablachat.component.user.entity.User;
-import pl.infobazasolution.blablachat.security.jwt.JWTTokenNeeded;
-
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.PathParam;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
 
 @Path("/test")
 public class TestController {
 
     @GET
     @Path("/ping")
-    @JWTTokenNeeded
     public String ping(){
         return LocalDateTime.now().toString();
     }
