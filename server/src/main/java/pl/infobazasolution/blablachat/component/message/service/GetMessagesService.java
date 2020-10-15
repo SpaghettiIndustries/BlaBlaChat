cdc78fa8a901e9b022de88f203c2bee39ec8db49
praktyka2 <praktyka2@info-baza.pl>
2020-10-15 08:46:34 +0200

    TASK-J-10 Oznaczanie wiadomości jako odczytane poprzez ustawienie właściwości readAt

--- server/src/main/java/pl/infobazasolution/blablachat/component/message/service/GetMessagesService.java
+++ server/src/main/java/pl/infobazasolution/blablachat/component/message/service/GetMessagesService.java
@@ -1,11 +1,16 @@
 package pl.infobazasolution.blablachat.component.message.service;
 
+import io.jsonwebtoken.Jwts;
 import pl.infobazasolution.blablachat.component.message.dao.MessageDao;
 import pl.infobazasolution.blablachat.component.message.dto.MessageDto;
 import pl.infobazasolution.blablachat.component.message.dto.MessageFilter;
 import pl.infobazasolution.blablachat.component.message.entity.Message;
+import pl.infobazasolution.blablachat.component.user.session.UserSession;
 
 import javax.inject.Inject;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.Context;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Objects;
@@ -16,6 +21,9 @@ public class GetMessagesService {
     @Inject
     private MessageDao messageDao;
 
+    @Inject
+    private UserSession userSession;
+
     public List<MessageDto> get(MessageFilter filter) {
         List<Message> messageEntities = messageDao.findRecent(filter);
 
@@ -23,7 +31,7 @@ public class GetMessagesService {
             List<MessageDto> messages = messageEntities.stream().map(message -> {
                 MessageDto messageDto = new MessageDto();
 
-                if (Objects.isNull(message.getReadAt())) {
+                if (!message.getSender().equals(userSession.getId()) && Objects.isNull(message.getReadAt())) {
                   message.setReadAt(ZonedDateTime.now());
 
                   Message updatedReadMessage = messageDao.update(message.getId(), message).get();
